a7e4d83ca275bb3d02b82b18c7599f2e0ea84e1c
Olivier Louvignes <olivier@mgcrea.io>
2020-11-10 20:00:04 +0100

    chore(eslint): add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'prettier/@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    // '@typescript-eslint/no-unused-vars': 'off',
+  },
+};
